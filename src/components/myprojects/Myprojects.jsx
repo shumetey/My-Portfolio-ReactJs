256d7fb857e50ce38eb8ff9d178a50e4ab08746c
shumetey <shumetey12@gmail.com>
2024-12-08 18:49:26 +0300

    After adding projects

--- src/components/myprojects/Myprojects.jsx
+++ src/components/myprojects/Myprojects.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import './myprojects.css'
 import IMG1 from '../../assets/TSL.png'
 import IMG2 from '../../assets/CMS.jpg'
-import IMG3 from '../../assets/img-1.jpg'
-import IMG4 from '../../assets/img-1.jpg'
+import IMG3 from '../../assets/JobPortal.JPG'
+import IMG4 from '../../assets/IQ.JPG'
+import IMG5 from '../../assets/Meyla.JPG'
+import IMG6 from '../../assets/Pentech.JPG'
 
 const Myprojects = () => {
   return (
@@ -38,25 +40,48 @@ const Myprojects = () => {
         </article>
         <article className='portfolio__item'>
             <div className="portfolio__item-image">
-                <img src={IMG4} alt="" />
+                <img src={IMG3} alt="" />
             </div>
-            <h3>My Portfolio(This website)</h3>
-            <small className='text-light'>ReactJs</small>
+            <h3>Job Portal System</h3>
+            <small className='text-light'>VueJS | TailwindCSS | NodeJs | Express | MySQL</small>
             <div className="portfolio__item-cta">
-              <a href="https://github.com/shumetey/My-Portfolio-ReactJs" target="_blank" rel='noreferrer' className='btn'>Github</a>
-              <a href="https://github.com/shumetey/My-Portfolio-ReactJs/" target="_blank" rel='noreferrer' className='btn btn-primary'>Visit Website</a>  
+              <a href="https://github.com/shumetey/Job-Portal-System-Frontend" target="_blank" rel='noreferrer' className='btn'>Github</a> 
             </div>
             
         </article>
         <article className='portfolio__item'>
             <div className="portfolio__item-image">
-                <img src={IMG3} alt="" />
+                <img src={IMG4} alt="" />
             </div>
-            <h3>My Resume</h3>
-            <small className='text-light'>React | TailwindCSS | JS | NodeJs</small>
+            <h3>IQ Business Group(Website)</h3>
+            <small className='text-light'>VueJS | TailwindCSS | NodeJs | Express | MySQL</small>
             <div className="portfolio__item-cta">
-              <a href="https://github.com/shumetey/My-Portfolio-ReactJs" target="_blank" rel='noreferrer' className='btn'>Github</a>
-              <a href="https://github.com/shumetey/My-Portfolio-ReactJs/" target="_blank" rel='noreferrer' className='btn btn-primary'>Visit Website</a>  
+              <a href="https://github.com/shumetey/IQ-Website-Frontend" target="_blank" rel='noreferrer' className='btn'>Github</a>
+              <a href="https://iq-businesses.com/" target="_blank" rel='noreferrer' className='btn btn-primary'>Visit Website</a>   
+            </div>
+            
+        </article>
+        <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+                <img src={IMG5} alt="" />
+            </div>
+            <h3>Meyla SACCO</h3>
+            <small className='text-light'>VueJS | TailwindCSS | NodeJs | Express | MySQL</small>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/shumetey/Meyla-Website-Frontend" target="_blank" rel='noreferrer' className='btn'>Github</a> 
+              <a href="https://meyla.et" target="_blank" rel='noreferrer' className='btn btn-primary'>Visit Website</a>  
+            </div>
+            
+        </article>
+        <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+                <img src={IMG6} alt="" />
+            </div>
+            <h3>PenTech Trading Sc.</h3>
+            <small className='text-light'>VueJS | TailwindCSS | NodeJs | Express | MySQL</small>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/zedkbro/Pentech-Backend" target="_blank" rel='noreferrer' className='btn'>Github</a> 
+              <a href="https://pentechtradingsc.com/" target="_blank" rel='noreferrer' className='btn btn-primary'>Visit Website</a>  
             </div>
             
         </article>
